139d078219542723a4f19395fd0ea9a916df0dda
felipequentino <felipequentino22@gmail.com>
2024-05-14 19:22:38 -0300

    :wastebasket: remove: Remoção do arquivo padrão do Vue HelloWorld

--- web-data/src/App.vue
+++ web-data/src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <LiveChart />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import LiveChart from './components/LiveChart.vue'
 
 export default {
   name: 'App',
   components: {
-    LiveChart,
-    HelloWorld
+    LiveChart
   }
 }
 </script>
