e4e75d4d5b0bd7c5e89ee8978c70eb0f4e18ca83
felipequentino <felipequentino22@gmail.com>
2024-05-14 19:20:47 -0300

    :sparkles: feat: Conexão com o arquivo js

--- web-data/src/components/LiveChart.vue
+++ web-data/src/components/LiveChart.vue
@@ -1,60 +1,22 @@
 <template>
-    <div>
-       <h1>Real-Time Random Number Display</h1>
-       <p>Current Random Number: {{ randomNumber }}</p>
-    </div>
-    <Bar
-    id="live-chart" 
-    :options="chartOptions"
-    :data="chartData"
-    />
-   </template>
-   
-   <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-   export default {
-    name: 'LiveChart',
-    components: { Bar },
-    computed: {
-        chartData() {
-            return {
-                labels: ['Random Number'],
-                datasets: [
-                    {
-                        label: 'Random Number',
-                        backgroundColor: '#f87979',
-                        data: [this.randomNumber]
-                    }
-                ]
-            };
-        },        
-    },
-    data() {
-       return {
-         randomNumber: 0
-       };
-    },
-
-    mounted() {
-       this.generateRandomNumber();
-       this.interval = setInterval(this.generateRandomNumber, 1000);
-    },
-    beforeUnmount() {
-       clearInterval(this.interval);
-    },
-    methods: {
-       generateRandomNumber() {
-         this.randomNumber = Math.floor(Math.random() * 100) + 1;
-       }
-    },
-   };
-   </script>
-   
-   <style scoped>
-   /* Add your CSS styling here if needed */
-   </style>
-   
+   <div>
+     <h1>Real-Time Population Change Display</h1>
+     <p>Current Population Change: {{ Math.round(currentPopulationChange) }}</p>
+     <Bar
+       id="live-chart"
+       :options="chartOptions"
+       :data="chartData"
+     />
+   </div>
+ </template>
+ 
+ <script>
+ import liveChart from './liveChart.js';
+ 
+ export default liveChart;
+ </script>
+ 
+ <style scoped>
+ /* Add your CSS styling here if needed */
+ </style>
+ 
