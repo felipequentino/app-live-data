18aa1235f75385083fbc4011328ee355b4524a8d
felipequentino <felipequentino22@gmail.com>
2024-06-03 16:22:55 -0300

    :bricks: ci: Adicionado os componentes e páginas no App.vue

--- web-data/src/App.vue
+++ web-data/src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <LiveChart />
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <NavbarComponent />
+    <component :is="currentPage"></component>
+    <FooterComponent />
+  </div>
 </template>
 
 <script>
-import LiveChart from './components/LiveChart.vue'
+import NavbarComponent from './components/NavbarComponent.vue';
+import HomePage from './pages/HomePage.vue';
+import ContatoPage from './pages/ContatoPage.vue';
+import SobrePage from './pages/SobrePage.vue';
+import FonteDeDadosPage from './pages/FonteDeDadosPage.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    LiveChart
+    NavbarComponent,
+    FooterComponent,
+    FonteDeDadosPage,
+    HomePage,
+    ContatoPage,
+    SobrePage
+  },
+  data() {
+    return {
+      currentPage: 'HomePage' // Página padrão
+    };
+  },
+  created() {
+    this.handlePopState();
+    window.addEventListener('popstate', this.handlePopState);
+  },
+  methods: {
+    navigateTo(page) {
+      this.currentPage = page;
+      history.pushState(null, '', page === 'HomePage' ? '/' : `/${page.toLowerCase()}`);
+    },
+    handlePopState() {
+      const path = window.location.pathname.substring(1);
+      if (path === 'contato') {
+        this.currentPage = 'ContatoPage';
+      } else if (path === 'sobre') {
+        this.currentPage = 'SobrePage';
+      
+      } else if (path === 'fonte-de-dados') {
+        this.currentPage = 'FonteDeDadosPage';
+      } else {
+        this.currentPage = 'HomePage';
+      }
+    }
+  },
+  beforeUnmount() {
+    window.removeEventListener('popstate', this.handlePopState);
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 }
 </style>
