c8e1c85caf8ce86b8e2c27473c43b91333027572
felipequentino <felipequentino22@gmail.com>
2024-06-03 16:23:46 -0300

    ::heavy_plus_sign:: build: adicionado dependências das libs do vue no main.js

--- web-data/src/main.js
+++ web-data/src/main.js
@@ -1,4 +1,32 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, fa } from 'vuetify/iconsets/fa-svg';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon); // Register component globally
+library.add(fas); // Include needed solid icons
+library.add(far); // Include needed regular icons
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+});
+
+app.use(vuetify);
+app.mount('#app');
