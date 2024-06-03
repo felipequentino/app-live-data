7ac401559c2e5d33678e84626c946a7767dc3ad1
DEVLevid <levidlima80@gmail.com>
2024-06-03 18:56:33 -0300

    🎉 feat: Updating PopulationDisplay component for improve on-screen availability.

--- web-data/src/components/PopulationDisplay.vue
+++ web-data/src/components/PopulationDisplay.vue
@@ -1,29 +1,29 @@
 <template>
   <v-container>
-    <div class="population-display">
+    <div class="displayContainer">
       <h2 class="title">Informações do Brasil atualizadas em tempo real</h2>
-      <v-row>
-        <v-col cols="12" md="6">
+      <v-col class="container">
+        <v-col class="cardContainer">
           <v-card class="section population-section">
             <v-card-title class="section-title">População</v-card-title>
             <v-card-text>
-              <p>População atual: {{ Math.round(currentPopulationChange) }}</p>
-              <p>Nascimentos hoje: {{ Math.round(birthsToday) }}</p>
+              <p>População atual: <span>{{ Math.round(currentPopulationChange) }}</span></p>
+              <p>Nascimentos hoje: <span>{{ Math.round(birthsToday) }}</span></p>
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col cols="12" md="6">
+        <v-col class="cardContainer">
           <v-card class="section economic-section">
             <v-card-title class="section-title">Econômico</v-card-title>
             <v-card-text>
-              <p>Pessoas empregadas: {{ Math.round(currentOccupationChange) }}</p>
-              <p>Pessoas desempregadas: {{ Math.round(currentUnemployedChange) }}</p>
-              <p>Valor médio do m² no Brasil: {{ Math.round(currentMetroChange) }}</p>
-              <p>Valor médio do que um brasileiro ganha: {{ Math.round(currentIncomeChange) }}</p>
+              <p>Pessoas empregadas: <span>{{ Math.round(currentOccupationChange) }}</span></p>
+              <p>Pessoas desempregadas: <span>{{ Math.round(currentUnemployedChange) }}</span></p>
+              <p>Valor médio do m² no Brasil: R$ <span>{{ Math.round(currentMetroChange) }}</span></p>
+              <p>Média salarial: R$ <span>{{ Math.round(currentIncomeChange) }}</span></p>
             </v-card-text>
           </v-card>
         </v-col>
-      </v-row>
+      </v-col>
     </div>
   </v-container>
 </template>
@@ -38,38 +38,55 @@ export default {
 </script>
 
 <style scoped>
-.population-display {
-  flex: 1;
-  padding: 40px;
+.container {
+  z-index: -10;
+  margin-top: 2rem;
+  display: grid;
+  place-items: center;
+}
+
+.cardContainer {
+  max-width: 60rem;
+}
+
+.displayContainer {
+  display: flex;
+  flex-direction: column;
   text-align: center;
+  margin: 2rem;
 }
 
 .title {
   text-align: center;
-  margin-bottom: 20px;
-  color: #003366; /* Azul mais escuro */
-  font-size: 2em; /* Letras maiores */
+  color: #003366;
+  font-size: 2.5em;
+  font-weight: bold;
+  margin-top: 2rem;
 }
 
 .section {
-  margin-bottom: 20px;
-  background-color: #ffffff; /* Verde escuro */
+  margin-bottom: 1rem;
+  background-color: #ffffff;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 .section-title {
-  background-color: #003366; /* Azul mais escuro */
+  background-color: #003366;
   color: white;
-  padding: 10px;
+  padding: 0.5rem;
   border-radius: 8px 8px 0 0;
-  font-size: 1.5em; /* Letras maiores */
+  font-size: 1.5em;
 }
 
 .section p {
-  margin: 0;
-  padding: 10px;
-  color: #000000; /* Letras mais escuras */
-  font-size: 1.2em; /* Letras maiores */
+  margin: 1rem;
+  color: #000000;
+  font-size: 1.5em;
+  font-weight: 600;
+}
+
+span {
+  color: rgb(17, 148, 63);
 }
 </style>
