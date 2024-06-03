28c4239644d658c8189fb35c613f3db56729a0c2
DEVLevid <levidlima80@gmail.com>
2024-06-03 18:52:16 -0300

    🚚 chore: Refactoring libraries, removing comments and improving code visibility.

--- web-data/src/main.js
+++ web-data/src/main.js
@@ -9,12 +9,12 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { faB } from '@fortawesome/free-solid-svg-icons';
 
 const app = createApp(App);
 
-app.component('font-awesome-icon', FontAwesomeIcon); // Register component globally
-library.add(fas); // Include needed solid icons
-library.add(far); // Include needed regular icons
+app.component('font-awesome-icon', FontAwesomeIcon); 
+library.add(fas, far, faB);
 
 const vuetify = createVuetify({
   components,
