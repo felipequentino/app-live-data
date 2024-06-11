21ba4401ae2856ff9a3d1bf571aa1d50805a72f4
felipequentino <felipequentino22@gmail.com>
2024-06-10 22:36:59 -0300

    :rocket: deploy: aplicação transformada em um PWA

--- web-data/src/main.js
+++ web-data/src/main.js
@@ -9,12 +9,13 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons'; 
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import { createRouter, createWebHistory } from 'vue-router';
 import SobrePage from './pages/SobrePage.vue';
 import ContatoPage from './pages/ContatoPage.vue';
 import FonteDeDadosPage from './pages/FonteDeDadosPage.vue';
 import HomePage from './pages/HomePage.vue';
+import './registerServiceWorker'
 
 const app = createApp(App);
 
@@ -31,7 +32,7 @@ const router = createRouter({
   routes,
 });
 
-app.component('font-awesome-icon', FontAwesomeIcon); 
+app.component('font-awesome-icon', FontAwesomeIcon);
 library.add(fas, far, fab);
 
 const vuetify = createVuetify({
--- web-data/vue.config.js
+++ web-data/vue.config.js
@@ -1,4 +1,47 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Ludiico Live',
+    themeColor: '#003366',
+    msTileColor: '#11943f',
+    manifestOptions: {
+      background_color: '#003366'
+    },
+    "icons": [
+      {
+        "src": "./img/icons/android-launchericon-48-48.png",
+        "sizes": "48x48",
+        "type": "image/png"
+      },
+      {
+        "src": "./img/icons/android-launchericon-72-72.png",
+        "sizes": "72x72",
+        "type": "image/png"
+      },
+        {
+            "src": "./img/icons/android-launchericon-96-96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+        },
+        {
+            "src": "./img/icons/android-launchericon-144-144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+        },
+        {
+            "src": "./img/icons/android-launchericon-192-192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "./img/icons/android-launchericon-512-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ],
+    workboxOptions: {
+
+    }
+  }
 })
