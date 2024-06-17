f0cf460fa025ac8cdcd27c12cf09275d7ca968ff
felipequentino <felipequentino22@gmail.com>
2024-06-17 12:03:27 -0300

    :sparkles: feat: adicionado tooltip em todas as informações

--- web-data/src/components/PopulationDisplay.vue
+++ web-data/src/components/PopulationDisplay.vue
@@ -2,61 +2,106 @@
   <v-container>
     <div class="displayContainer">
       <h2 class="title">Informações do Brasil atualizadas em tempo real</h2>
-      <v-col class="container">
-        <v-col class="cardContainer">
+      <v-row class="container">
+        <v-col class="cardContainer" cols="12" md="4">
           <v-card class="section population-section">
             <v-card-title class="section-title">População</v-card-title>
             <v-card-text>
-              <p>População atual: <span>{{ Math.round(currentPopulationChange) }}</span></p>
-              <p>Nascimentos esse ano <span>{{ Math.round(birthsThisYear) }}</span></p>
-              <p>Nascimentos hoje: <span>{{ Math.round(birthsToday) }}</span></p>
-              <p>Mortes esse ano: <span>{{ Math.round(deathsThisYear) }}</span></p>
-              <p>Mortes hoje: <span>{{ Math.round(deathsToday) }}</span></p>
+              <br />
+              <TooltipComponent displayText="População atual: " tooltipText=" População - Total" />
+              <span>{{ Math.round(currentPopulationChange) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Nascimentos esse ano: " tooltipText="Nascidos vivos - Total no ano de 2024" />
+              <span>{{ Math.round(birthsThisYear) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Nascimentos hoje: " tooltipText="Nascidos vivos - Hoje" />
+              <span>{{ Math.round(birthsToday) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Mortes esse ano: " tooltipText="Óbitos - Total no ano de 2024" />
+              <span>{{ Math.round(deathsThisYear) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Mortes hoje: " tooltipText="Óbitos - Hoje" />
+              <span>{{ Math.round(deathsToday) }}</span>
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col class="cardContainer">
+        <v-col class="cardContainer" cols="12" md="4">
           <v-card class="section economic-section">
             <v-card-title class="section-title">Econômico</v-card-title>
             <v-card-text>
-              <p>Pessoas empregadas: <span>{{ Math.round(currentOccupationChange) }}</span></p>
-              <p>Pessoas desempregadas: <span>{{ Math.round(currentUnemployedChange) }}</span></p>
-              <p>Média salarial: R$ <span>{{ Math.round(currentIncomeChange) }}</span></p>
-              <p>Valor médio do m² no Brasil: R$ <span>{{ currentMetroChange.toFixed(2) }}</span></p>
-              <p>Rendimento mensal de todos os brasileiros: R$ <span>{{ currentIncomeMassaChange.toFixed(2) }}</span></p>
+              <br />
+              <TooltipComponent displayText="Pessoas empregadas: " tooltipText="Pessoas de 14 anos ou mais de idade, ocupadas na semana de referência"></TooltipComponent>
+              <span>{{ Math.round(currentOccupationChange) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Pessoas desempregadas: " tooltipText="Pessoas de 14 anos ou mais de idade, desocupadas nas semana de referência" />
+              <span>{{ Math.round(currentUnemployedChange) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Valor médio do m² no Brasil: " tooltipText="Custo médio m² em moeda corrente" />
+              <span>R$ {{ currentMetroChange.toFixed(2) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Média salarial: " tooltipText="Rendimento médio real do trabalho principal, habitualmente recebido por mês, pelas pessoas de 14 anos ou mais de idade, ocupadas na semana de referência, com rendimento de trabalho" />
+              <span>R$ {{ Math.round(currentIncomeChange) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Rendimento mensal de todos os brasileiros: " tooltipText="Massa de rendimento real de todos os trabalhos, habitualmente recebido por mês, pelas pessoas de 14 anos ou mais de idade, ocupadas na semana de referência, com rendimento de trabalho" />
+              <span>R$ {{ currentIncomeMassaChange.toFixed(2) }}</span>
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col class="cardContainer">
+        <v-col class="cardContainer" cols="12" md="4">
           <v-card class="section regions-section">
             <v-card-title class="section-title">Regiões</v-card-title>
             <v-card-text>
-              <p>Valor médio do m² no Centro-Oeste: R$ <span>{{ currentCentroOesteMetroChange.toFixed(2) }}</span></p>
-              <p>Valor médio do m² no Nordeste: R$ <span>{{ currentNordesteMetroChange.toFixed(2) }}</span></p>
-              <p>Valor médio do m² no Sudeste: R$ <span>{{ currentSudesteMetroChange.toFixed(2) }}</span></p>
-              <p>Valor médio do m² no Norte: R$ <span>{{ currentNorteMetroChange.toFixed(2) }}</span></p>
-              <p>Valor médio do m² no Sul: R$ <span>{{ currentSulMetroChange.toFixed(2) }}</span></p>
-
+              <br />
+              <TooltipComponent displayText="Valor médio do m² no Centro-Oeste: " tooltipText="Custo médio m² em moeda corrente do Centro-Oeste" />
+              <span>R$ {{ currentCentroOesteMetroChange.toFixed(2) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Valor médio do m² no Nordeste: " tooltipText="Custo médio m² em moeda corrente do Nordeste" />
+              <span>R$ {{ currentNordesteMetroChange.toFixed(2) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Valor médio do m² no Sudeste: " tooltipText="Custo médio m² em moeda corrente do Sudeste" />
+              <span>R$ {{ currentSudesteMetroChange.toFixed(2) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Valor médio do m² no Norte: " tooltipText="Custo médio m² em moeda corrente do Norte" />
+              <span>R$ {{ currentNorteMetroChange.toFixed(2) }}</span>
+              <br />
+              <br />
+              <TooltipComponent displayText="Valor médio do m² no Sul: " tooltipText="Custo médio m² em moeda corrente do Sul" />
+              <span>R$ {{ currentSulMetroChange.toFixed(2) }}</span>
             </v-card-text>
           </v-card>
         </v-col>
-      </v-col>
+      </v-row>
     </div>
   </v-container>
 </template>
 
 <script>
 import liveChart from './liveChart.js';
+import TooltipComponent from './TooltipComponent.vue';
 
 export default {
   ...liveChart,
+  components: {
+    TooltipComponent,
+  },
   name: 'PopulationDisplay',
+
 };
 </script>
 
 <style scoped>
 .container {
-  z-index: -10;
   margin-top: 2rem;
   display: grid;
   place-items: center;
@@ -105,5 +150,8 @@ export default {
 
 span {
   color: rgb(17, 148, 63);
+  font-size: 1.5em;
+  font-weight: 600;
+  margin: 0.5rem;
 }
 </style>
---
+++ web-data/src/components/TooltipComponent.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="tooltip-container">
+      <span @mouseover="showTooltip" @mouseleave="hideTooltip" @click="toggleTooltip">{{ displayText }}</span>
+      <div v-if="isTooltipVisible" class="tooltip">
+        {{ tooltipText }}
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TooltipComponent',
+    props: {
+      displayText: {
+        type: String,
+        required: true
+      },
+      tooltipText: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        isTooltipVisible: false
+      };
+    },
+    methods: {
+      showTooltip() {
+        this.isTooltipVisible = true;
+      },
+      hideTooltip() {
+        this.isTooltipVisible = false;
+      },
+      toggleTooltip() {
+        this.isTooltipVisible = !this.isTooltipVisible;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .tooltip-container {
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+  }
+  
+  .tooltip-container span {
+    color: #000000;
+    font-size: 1.5em;
+    font-weight: 600;
+  }
+  
+  .tooltip {
+    visibility: visible;
+    background-color: #000;
+    color: #fff;
+    text-align: center;
+    border-radius: 5px;
+    padding: 5px;
+    position: absolute;
+    z-index: 1;
+    bottom: 150%; /* Adjusted to give a bit more space between text and tooltip */
+    left: 50%;
+    transform: translateX(-50%);
+    opacity: 0.9;
+    max-width: 500px; /* Maximum width of the tooltip */
+    min-width: 290px; /* Minimum width of the tooltip */
+    display: inline-flex; /* Use flexbox for wrapping content */
+    flex-wrap: wrap; /* Allow text to wrap if necessary */
+    justify-content: center; /* Center the text content */
+    white-space: normal; /* Allow text to wrap */
+    word-wrap: break-word; /* Ensure long words break appropriately */
+  }
+  
+  .tooltip::after {
+    content: '';
+    position: absolute;
+    top: 100%; /* Arrow at the bottom of the tooltip */
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: black transparent transparent transparent; /* Arrow color */
+  }
+  </style>
+  
