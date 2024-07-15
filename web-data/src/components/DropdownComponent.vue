3c363f5d9695184818b415ea80095956319be69b
felipequentino <felipequentino22@gmail.com>
2024-07-14 21:51:19 -0300

    :sparkles: feat: criação de um dropdown content substituição do component do tooltip text por um dropdown content

---
+++ web-data/src/components/DropdownComponent.vue
@@ -0,0 +1,153 @@
+<template>
+    <div class="dropdown">
+      <div class="dropdown-title" @click="toggleDropdown">
+        <!-- Número e título alinhados horizontalmente em desktop -->
+        <div class="title-container" v-if="!isMobile">
+          <span class="number" style="color: rgb(17, 148, 63);">{{ number }}</span>
+          <span class="title">{{ title }}</span>
+        </div>
+        <!-- Número acima do título em dispositivos móveis -->
+        <div class="title-container" v-else>
+          <span class="number" style="color: rgb(17, 148, 63);">{{ number }}</span>
+          <span class="title">{{ title }}</span>
+        </div>
+        <!-- Ícone de expansão/retração -->
+        <span v-if="!isOpen">[+]</span>
+        <span v-else>[-]</span>
+      </div>
+      <transition name="slide">
+        <div class="dropdown-content" v-show="isOpen">
+          <p>{{ description }}</p>
+        </div>
+      </transition>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "DropdownContent",
+    props: {
+      number: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        isOpen: false,
+        isMobile: false // Flag para verificar se é mobile
+      };
+    },
+    mounted() {
+      // Verifica se a largura da janela é menor que 768px (exemplo para dispositivos móveis)
+      this.isMobile = window.innerWidth < 768;
+      // Adiciona um listener de redimensionamento para atualizar a flag isMobile
+      window.addEventListener('resize', this.checkMobile);
+    },
+    unmounted() {
+      // Remove o listener de redimensionamento ao destruir o componente
+      window.removeEventListener('resize', this.checkMobile);
+    },
+    methods: {
+      toggleDropdown() {
+        this.isOpen = !this.isOpen;
+      },
+      checkMobile() {
+        this.isMobile = window.innerWidth < 768;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .dropdown {
+    margin-bottom: 10px;
+  }
+  
+  .dropdown-title {
+    cursor: pointer;
+    background-color: #f5f5f5;
+    padding: 10px;
+    border: 1px solid #ddd;
+    display: flex; /* Tornando o título um contêiner flexível */
+    justify-content: space-between; /* Colocando espaço máximo entre o título e o sinal */
+  }
+  
+  .title-container {
+  display: flex;
+  flex-direction: row; /* Alinha o número e o título horizontalmente */
+  position: relative; /* Necessário para posicionar o título absolutamente */
+  width: 100%; /* Ocupa toda a largura disponível */
+}
+
+.number {
+  font-size: 1.5em; /* Tamanho do número */
+  font-weight: bold;
+  flex-shrink: 0; /* Impede que o número encolha e afete a posição do título */
+}
+
+.title {
+  font-size: 1.2em; /* Tamanho do título */
+  position: absolute; /* Posiciona o título absolutamente */
+  left: 60%; /* Posiciona o título um pouco à direita do centro */
+}
+
+@media (max-width: 767px) {
+  .title-container {
+    flex-direction: column; /* Alterado para coluna para mobile */
+    align-items: flex-start; /* Alinha o número e o título ao início do contêiner */
+  }
+
+  .title {
+    position: static; /* Remove a posição absoluta na visualização mobile */
+    left: auto; /* Remove o deslocamento à direita na visualização mobile */
+  }
+}
+
+
+  
+  .dropdown-content {
+    overflow: hidden;
+    border: 1px solid #ddd;
+    border-top: none;
+    padding: 10px;
+    background-color: #fff;
+  }
+  
+  /* Definindo a animação de transform */
+  @keyframes slide {
+    from {
+      transform: scaleY(0);
+      transform-origin: top;
+    }
+    to {
+      transform: scaleY(1);
+      transform-origin: top;
+    }
+  }
+  
+  .slide-enter-active, .slide-leave-active {
+    animation-name: slide;
+    animation-duration: 0.5s;
+    animation-fill-mode: forwards; /* Garante que a animação termine na posição final */
+  }
+  
+  /* Aplicando a animação de entrada */
+  .slide-enter {
+    animation-play-state: running;
+  }
+  
+  /* Aplicando a animação de saída */
+  .slide-leave-active {
+    animation-direction: reverse;
+  }
+  </style>
+  
--- web-data/src/components/PopulationDisplay.vue
+++ web-data/src/components/PopulationDisplay.vue
@@ -8,32 +8,47 @@
             <v-card-title class="section-title">População</v-card-title>
             <v-card-text>
               <br />
-              <TooltipComponent displayText="População atual: " tooltipText=" População - Total" />
-              <span>{{ Math.round(Math.round(currentPopulationMenChange) + Math.round(currentPopulationWomenChange)).toLocaleString('pt-BR') }}</span>
+              <DropdownContent
+                :number="Math.round(currentPopulationMenChange + currentPopulationWomenChange).toLocaleString('pt-BR')"
+                title="População Total"
+                description="População - Total"
+              />
               <br />
+              <DropdownContent
+                :number="Math.round(currentPopulationMenChange).toLocaleString('pt-BR')"
+                title="População de Homens"
+                description="População de Homens - Total"
+              />
               <br />
-              <TooltipComponent displayText="População de Homens: " tooltipText=" População de Homens - Total" />
-              <span>{{ Math.round(currentPopulationMenChange).toLocaleString('pt-BR') }}</span>
+              <DropdownContent
+                :number="Math.round(currentPopulationWomenChange).toLocaleString('pt-BR')"
+                title="População de Mulheres"
+                description="População de Mulheres - Total"
+              />
               <br />
+              <DropdownContent
+                :number="Math.round(birthsThisYear).toLocaleString('pt-BR')"
+                title="Nascimentos esse ano"
+                description="Nascidos vivos - Total no ano de 2024"
+              />
               <br />
-              <TooltipComponent displayText="População de Mulheres: " tooltipText=" População de Mulheres- Total" />
-              <span>{{ Math.round(currentPopulationWomenChange).toLocaleString('pt-BR') }}</span>
+              <DropdownContent
+                :number="Math.round(birthsToday).toLocaleString('pt-BR')"
+                title="Nascimentos hoje"
+                description="Nascidos vivos - Hoje"
+              />
               <br />
+              <DropdownContent
+                :number="Math.round(deathsThisYear).toLocaleString('pt-BR')"
+                title="Mortes esse ano"
+                description="Óbitos - Total no ano de 2024"
+              />
               <br />
-              <TooltipComponent displayText="Nascimentos esse ano: " tooltipText="Nascidos vivos - Total no ano de 2024" />
-              <span>{{ Math.round(birthsThisYear).toLocaleString('pt-BR') }}</span>
-              <br />
-              <br />
-              <TooltipComponent displayText="Nascimentos hoje: " tooltipText="Nascidos vivos - Hoje" />
-              <span>{{ Math.round(birthsToday).toLocaleString('pt-BR') }}</span>
-              <br />
-              <br />
-              <TooltipComponent displayText="Mortes esse ano: " tooltipText="Óbitos - Total no ano de 2024" />
-              <span>{{ Math.round(deathsThisYear).toLocaleString('pt-BR') }}</span>
-              <br />
-              <br />
-              <TooltipComponent displayText="Mortes hoje: " tooltipText="Óbitos - Hoje" />
-              <span>{{ Math.round(deathsToday).toLocaleString('pt-BR') }}</span>
+              <DropdownContent
+                :number="Math.round(deathsToday).toLocaleString('pt-BR')"
+                title="Mortes hoje"
+                description="Óbitos - Hoje"
+              />
             </v-card-text>
           </v-card>
         </v-col>
@@ -42,29 +57,41 @@
             <v-card-title class="section-title">Empregos</v-card-title>
             <v-card-text>
               <br />
-              <TooltipComponent displayText="Pessoas empregadas: " tooltipText="Pessoas de 14 anos ou mais de idade, ocupadas na semana de referência"></TooltipComponent>
-              <span>{{  Math.round(Math.round(currentOccupationMenChange) + Math.round(currentOccupationWomenChange)).toLocaleString('pt-BR') }}</span>
+              <DropdownContent
+                :number="Math.round(currentOccupationMenChange + currentOccupationWomenChange).toLocaleString('pt-BR')"
+                title="Pessoas empregadas"
+                description="Pessoas de 14 anos ou mais de idade, ocupadas na semana de referência"
+              />
               <br />
+              <DropdownContent
+                :number="Math.round(currentOccupationMenChange).toLocaleString('pt-BR')"
+                title="Homens empregados"
+                description="Homens de 14 anos ou mais de idade, ocupados na semana de referência"
+              />
               <br />
-              <TooltipComponent displayText="Homens empregados: " tooltipText="Homens de 14 anos ou mais de idade, ocupados na semana de referência"></TooltipComponent>
-              <span>{{ Math.round(currentOccupationMenChange).toLocaleString('pt-BR') }}</span>
+              <DropdownContent
+                :number="Math.round(currentOccupationWomenChange).toLocaleString('pt-BR')"
+                title="Mulheres empregadas"
+                description="Mulheres de 14 anos ou mais de idade, ocupadas na semana de referência"
+              />
               <br />
+              <DropdownContent
+                :number="Math.round(currentUnemployedMenChange + currentUnemployedWomenChange).toLocaleString('pt-BR')"
+                title="Pessoas desempregadas"
+                description="Pessoas de 14 anos ou mais de idade, desocupadas na semana de referência"
+              />
               <br />
-              <TooltipComponent displayText="Mulheres empregadas: " tooltipText="Mulheres de 14 anos ou mais de idade, ocupadas na semana de referência"></TooltipComponent>
-              <span>{{ Math.round(currentOccupationWomenChange).toLocaleString('pt-BR') }}</span>
+              <DropdownContent
+                :number="Math.round(currentUnemployedMenChange).toLocaleString('pt-BR')"
+                title="Homens desempregados"
+                description="Homens de 14 anos ou mais de idade, desocupados na semana de referência"
+              />
               <br />
-              <br />
-              <TooltipComponent displayText="Pessoas desempregadas: " tooltipText="Pessoas de 14 anos ou mais de idade, desocupadas nas semana de referência" />
-              <span>{{  Math.round(Math.round(currentUnemployedMenChange) + Math.round(currentUnemployedWomenChange)).toLocaleString('pt-BR') }}</span>
-              <br />
-              <br />
-              <TooltipComponent displayText="Homens desempregados: " tooltipText="Homens de 14 anos ou mais de idade, desocupadas nas semana de referência" />
-              <span>{{ Math.round(currentUnemployedMenChange).toLocaleString('pt-BR') }}</span>
-              <br />
-              <br />
-              <TooltipComponent displayText="Mulheres desempregadas: " tooltipText="Mulheres de 14 anos ou mais de idade, desocupadas nas semana de referência" />
-              <span>{{ Math.round(currentUnemployedWomenChange).toLocaleString('pt-BR') }}</span>
-              <br />
+              <DropdownContent
+                :number="Math.round(currentUnemployedWomenChange).toLocaleString('pt-BR')"
+                title="Mulheres desempregadas"
+                description="Mulheres de 14 anos ou mais de idade, desocupadas na semana de referência"
+              />
               <br />
             </v-card-text>
           </v-card>
@@ -74,20 +101,29 @@
             <v-card-title class="section-title">Econômico</v-card-title>
             <v-card-text>
               <br>
-              <TooltipComponent displayText="Gastos com a saúde: " tooltipText="Total de despesas executadas para a área de atuação Saúde - Esse Ano (2024)" />
-              <span>R$ {{ currentHealthExpensesChange.toLocaleString('pt-BR', {style: 'decimal', minimumFractionDigits:2,maximumFractionDigits: 2}) }}</span>
+              <DropdownContent
+                :number="currentHealthExpensesChange.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })"
+                title="Gastos com a saúde"
+                description="Total de despesas executadas para a área de atuação Saúde - Esse Ano (2024)"
+              />
               <br />
+              <DropdownContent
+                :number="currentEducationExpensesChange.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })"
+                title="Gastos com a educação"
+                description="Total de despesas executadas para a área de atuação Educação -  Esse Ano (2024)"
+              />
               <br />
-              <TooltipComponent displayText="Gastos com a educação: " tooltipText="Total de despesas executadas para a área de atuação Educação -  Esse Ano (2024)" />
-              <span>R$ {{ currentEducationExpensesChange.toLocaleString('pt-BR', {style: 'decimal', minimumFractionDigits:2,maximumFractionDigits: 2}) }}</span>
+              <DropdownContent
+                :number="currentDefenseExpensesChange.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })"
+                title="Gastos com o Ministério da Defesa"
+                description="Total de despesas executadas para a área do Ministério da Defesa - DEFESA - Esse Ano (2024)"
+              />
               <br />
-              <br />
-              <TooltipComponent displayText="Gastos com o Ministério da Defesa: " tooltipText="Total de despesas executadas para a área do Ministério da Defesa - DEFESA - Esse Ano (2024)" />
-              <span>R$ {{ currentDefenseExpensesChange.toLocaleString('pt-BR', {style: 'decimal', minimumFractionDigits:2,maximumFractionDigits: 2}) }}</span>
-              <br />
-              <br />
-              <TooltipComponent displayText="Rendimento mensal de todos os brasileiros: " tooltipText="Massa de rendimento real de todos os trabalhos, habitualmente recebido por mês, pelas pessoas de 14 anos ou mais de idade, ocupadas na semana de referência, com rendimento de trabalho - Esse Ano (2024)" />
-              <span>R$ {{ currentIncomeMassaChange.toLocaleString('pt-BR', {style: 'decimal', minimumFractionDigits:2, maximumFractionDigits: 2}) }}</span>
+              <DropdownContent
+                :number="currentIncomeMassaChange.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })"
+                title="Rendimento de todos os brasileiros"
+                description="Massa de rendimento real de todos os trabalhos, habitualmente recebido por mês, pelas pessoas de 14 anos ou mais de idade, ocupadas na semana de referência, com rendimento de trabalho - Esse Ano (2024)"
+              />
             </v-card-text>
           </v-card>
         </v-col>
@@ -96,16 +132,23 @@
             <v-card-title class="section-title">Veículos</v-card-title>
             <v-card-text>
               <br />
-              <TooltipComponent displayText="Veículos Licenciados: " tooltipText="Veículos Licenciados - Esse Ano (2024)" />
-              <span>{{ Math.round(Math.round(currentLicenciadosNacionaisChange) + Math.round(currentImportacaoVeiculosChange)).toLocaleString('pt-BR') }}</span>
+              <DropdownContent
+                :number="Math.round(currentLicenciadosNacionaisChange + currentImportacaoVeiculosChange).toLocaleString('pt-BR')"
+                title="Veículos Licenciados"
+                description="Veículos Licenciados - Total no ano de 2024"
+              />
               <br />
+              <DropdownContent
+                title="Veículos Exportados do Brasil"
+                :number="Math.round(currentExportacaoVeiculosChange).toLocaleString('pt-BR')"
+                description="Veículos Exportados - Total no ano de 2024"
+              />
               <br />
-              <TooltipComponent displayText="Veículos Exportados do Brasil: " tooltipText="Veículos Exportados do Brasil - Esse Ano (2024)" />
-              <span>{{ Math.round(currentExportacaoVeiculosChange).toLocaleString('pt-BR') }}</span>
-              <br />
-              <br />
-              <TooltipComponent displayText="Veículos Produzidos no Brasil: " tooltipText="Veículos Produzidos no Brasil - Esse Ano (2024)" />
-              <span>{{ Math.round(currentProducaoVeiculosChange).toLocaleString('pt-BR') }}</span>
+              <DropdownContent
+                title="Veículos Produzidos no Brasil"
+                :number="Math.round(currentProducaoVeiculosChange).toLocaleString('pt-BR')"
+                description="Veículos Produzidos - Total no ano de 2024"
+              />
             </v-card-text>
           </v-card>
         </v-col>
@@ -116,15 +159,14 @@
 
 <script>
 import liveChart from './liveChart.js';
-import TooltipComponent from './TooltipComponent.vue';
+import DropdownContent from './DropdownComponent.vue';
 
 export default {
   ...liveChart,
   components: {
-    TooltipComponent,
+    DropdownContent
   },
   name: 'PopulationDisplay',
-
 };
 </script>
 
@@ -174,6 +216,13 @@ export default {
   color: #000000;
   font-size: 1.5em;
   font-weight: 600;
+}
+
+.dropdown-content p {
+  color: rgb(17, 148, 63);
+  font-size: 1.5em;
+  font-weight: 600;
+  margin: 0.5rem;
 }
 
 span {
